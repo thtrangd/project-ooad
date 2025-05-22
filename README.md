0ace36d2fbb2d541468147ceba3bd555341909aa
thtrangd <thuytrangd212@gmail.com>
2025-05-23 01:14:48 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# project-ooad
+booking cinema ticket
---
+++ main.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
